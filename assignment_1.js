48d2ecce8b59e81da8e5223d33cbe9318733231e
Muthamil2603 <muthamil80eee@gmail.com>
2025-09-28 19:20:18 +0530

    Assignment 1 - Condition - 'IF' statement

---
+++ assignment_1.js
@@ -0,0 +1,12 @@
+//Using If condition
+let browserName = "Chrome" ;
+function printBrowser()
+{
+    if(browserName=="Chrome")
+    console.log("The browser name is " + browserName);
+else if(browserName=="Firefox")
+    console.log("The browser name is " + browserName);
+else if(browserName=="Edge")
+    console.log("The browser name is " + browserName);
+}
+printBrowser();
