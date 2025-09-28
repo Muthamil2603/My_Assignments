3db260a7b85b88f5b3c31d3b88170e57e15be040
Muthamil2603 <muthamil80eee@gmail.com>
2025-09-28 19:40:19 +0530

    Assignment 2 - Print Odd numbers till 25

---
+++ assignment_2.js
@@ -0,0 +1,9 @@
+//Using FOR loop - Print Odd numbers till 25
+let i;
+function printOddNumbers()
+{
+for(i=0;i<=25;i++)
+    if(i%2==1)
+    console.log(i + " is an odd number");
+}
+printOddNumbers();
